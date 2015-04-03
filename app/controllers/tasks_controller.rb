04d396314cdbb718026d65a6eb5f6ae041ff9547
Xian Ran <xranthoar@gmail.com>
2015-04-03 11:33:22 +0800

    refresh task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,9 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
+    Task.where(finished_at: nil).each do |t|
+      t.update_from_server!
+    end
     @tasks = Task.all.desc(:created_at)
   end
 
